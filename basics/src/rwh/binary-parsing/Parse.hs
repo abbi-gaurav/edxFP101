52bad185740b416cce7764fb2d722fac0286eec6
Gaurav Abbi <gaurav.abbi@sap.com>
2018-04-08 16:09:31 +0200

    RWH - Functors
    - Add TreeMap example for functor
    - Implement functor for Parse
    - Use `fmap` for extending features of Parse

--- basics/src/rwh/binary-parsing/Parse.hs
+++ basics/src/rwh/binary-parsing/Parse.hs
@@ -1,7 +1,9 @@
 module Parse where
 
+import           Control.Applicative
 import qualified Data.ByteString.Lazy       as L
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.Char
 import           Data.Int
 import           Data.Word
 
@@ -63,3 +65,28 @@ firstParser ==> secondParser = Parse chainedParser
           case runParse firstParser initState of
             Left errMsg -> Left errMsg
             Right (firstResult, newState) -> runParse (secondParser firstResult) newState
+
+instance Functor Parse where
+  fmap f parser = parser ==> (\result -> identity (f result))
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+parseChar :: Parse Char
+parseChar = fmap w2c parseByte
+
+peekByte :: Parse (Maybe Word8)
+peekByte = let ps = getState
+               f  = fmap fst . L.uncons . string
+           in fmap f ps
+
+peekChar :: Parse (Maybe Char)
+--peekChar = fmap (fmap w2c) peekByte
+peekChar = (fmap w2c) <$> peekByte
+
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = ((fmap p) <$> peekByte) ==> \maybeTrue ->
+                                             if maybeTrue == Just True
+                                             then parseByte ==> \byte ->
+                                               (byte:) <$> parseWhile p
+                                             else identity []
