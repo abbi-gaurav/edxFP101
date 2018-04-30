fb2f9012cb20b8be2ed214f2fb0d722e1c70c350
Gaurav Abbi <gaurav.abbi@sap.com>
2018-04-30 19:38:11 +0200

    RWH - Move tests to test directory

---
+++ basics/test/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import qualified QC          as DocTests
+import           System.Exit
+
+main :: IO ()
+main = do
+  good <- and <$> (sequence [DocTests.runTests])
+  if good
+    then exitSuccess
+    else exitFailure
