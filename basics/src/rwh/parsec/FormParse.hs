e55cf31fb730073e3df59559ee23759732a2c61c
Gaurav Abbi <abbi.gaurav@gmail.com>
2018-08-12 17:15:39 +0200

    RWH - Compact p_pair using lift

--- basics/src/rwh/parsec/FormParse.hs
+++ basics/src/rwh/parsec/FormParse.hs
@@ -1,5 +1,6 @@
 module FormParse where
 
+import           Control.Monad
 import           Numeric
 import           Text.ParserCombinators.Parsec
 import           Text.ParserCombinators.Parsec.Char
@@ -8,10 +9,8 @@ p_query :: CharParser () [(String, Maybe String)]
 p_query = p_pair `sepBy` char '&'
 
 p_pair :: CharParser () (String, Maybe String)
-p_pair = do
-  name <- many1 p_char
-  value <- optionMaybe (char '=' >> many p_char)
-  return (name, value)
+p_pair = liftM2 (,) (many1 p_char) (optionMaybe (char '=' >> many p_char))
+
 
 p_char :: CharParser () Char
 p_char = oneOf urlBaseChars
