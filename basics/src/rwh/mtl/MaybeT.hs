17b7a3fc3c5f29e37852a2defe3c41f5b37b5da4
Gaurav Abbi <abbi.gaurav@gmail.com>
2018-08-19 16:14:31 +0200

    RWH - MTL - Build a MaybeT

---
+++ basics/src/rwh/mtl/MaybeT.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
+module MaybeT where
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.State
+import           Control.Monad.Trans
+
+newtype MaybeT m a = MaybeT {
+  runMaybeT :: m (Maybe a)
+  }
+
+bindMT :: (Monad m) => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+x `bindMT` f = MaybeT $ do
+  unwrapped <- runMaybeT x
+  case unwrapped of
+    Nothing -> return Nothing
+    Just y  -> runMaybeT (f y)
+
+altBindMT :: Monad m => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+x `altBindMT` f =
+  MaybeT $ runMaybeT x >>= maybe (return Nothing) (runMaybeT . f)
+
+returnMT :: (Monad m) => a -> MaybeT m a
+returnMT a = MaybeT $ return (Just a)
+
+failMT :: (Monad m) => t -> MaybeT m a
+failMT _ = MaybeT $ return Nothing
+
+instance Monad f => Functor (MaybeT f) where
+  fmap  = liftM
+
+instance Monad a => Applicative (MaybeT a) where
+  (<*>) = ap
+  pure = return
+
+instance Monad m => Monad (MaybeT m) where
+  return = returnMT
+  (>>=) = bindMT
+  fail = failMT
+
+instance MonadTrans MaybeT where
+  lift m = MaybeT (Just `liftM` m)
+
+instance (MonadIO m) => MonadIO (MaybeT m) where
+  liftIO m = lift (liftIO m)
+
+instance (MonadState s m) => MonadState s (MaybeT m) where
+  get = lift get
+  put k = lift (put k)
