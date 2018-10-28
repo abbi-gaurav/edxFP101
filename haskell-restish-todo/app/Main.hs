449b3bcac1c4c5b7264aabf685342c356ac48d09
Gaurav Abbi <abbi.gaurav@gmail.com>
2018-10-28 12:30:08 +0100

    Haskell - Servant - REST - Working basic command parsing

--- haskell-restish-todo/app/Main.hs
+++ haskell-restish-todo/app/Main.hs
@@ -1,6 +1,42 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import Lib
+import           Control.Monad       (join)
+import           Data.Semigroup      ((<>))
+import           Lib
+import           Options.Applicative (CommandFields, Mod, Parser, ParserInfo,
+                                      argument, command, execParser, idm, info,
+                                      str, subparser)
+
+-- Todo : improve with newtype? use existing types
+type Host = String
+type Port = Integer
+
+newtype Options = Options {cms :: Command}
+data Command = Serve Host Port
+
+-- | Start up the server and serve requests
+server :: IO ()
+server = putStrLn "<SERVER START>"
+
+-- | CLI options parser
+opts :: Parser (IO ())
+opts = subparser commands
+  where
+    -- | IO action that produces an IO action.
+    --   This was necessary due to using optparse-applicative like I am, by returning an IO action straight after `info` in serverCmd
+    serverAction :: Parser (IO ())
+    serverAction = pure server
+
+    serverCmd :: ParserInfo (IO ())
+    serverCmd = info (serverAction) idm
+
+    commands :: Mod CommandFields (IO ())
+    commands = command "server" serverCmd
 
 main :: IO ()
-main = someFunc
+main = join $ execParser parser
+  where
+    parser::ParserInfo (IO ())
+    parser = info opts idm
