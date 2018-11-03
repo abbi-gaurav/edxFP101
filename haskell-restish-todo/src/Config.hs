b904f3d4d17f14db7a1d22cee05d97a3a37500d7
Gaurav Abbi <abbi.gaurav@gmail.com>
2018-11-03 18:34:23 +0100

    REST - Implement config overriding
    **Description**
    Order is `Default -> Files (JSON, TOML) -> ENV`

--- haskell-restish-todo/src/Config.hs
+++ haskell-restish-todo/src/Config.hs
@@ -6,17 +6,23 @@
 
 module Config where
 
-import           Control.Monad         (join)
+import           Control.Applicative       ((<|>))
+import           Control.Exception         (Exception, throw, try)
+import           Control.Monad             (join, when)
 import           Data.Aeson
-import           Data.Aeson.Types      (parseEither)
-import           Data.Bifunctor        (bimap, first, second)
-import qualified Data.ByteString.Lazy  as DBL
+import           Data.Aeson.Types          (parseEither)
+import           Data.Bifunctor            (bimap, first, second)
+import qualified Data.ByteString.Lazy      as DBL
 import           Data.Functor.Identity
-import qualified Data.Text.IO          as DTI
+import           Data.Maybe                (fromMaybe)
+import           Data.Text
+import qualified Data.Text.IO              as DTI
+import qualified Filesystem.Path           as FP
+import qualified Filesystem.Path.CurrentOS as FCOS
 import           GHC.Generics
-import           Text.Parsec.Error     (ParseError)
-import           Text.Read             (readMaybe)
-import           Text.Toml             (parseTomlDoc)
+import           Text.Parsec.Error         (ParseError)
+import           Text.Read                 (readMaybe)
+import           Text.Toml                 (parseTomlDoc)
 
 data AppConfig = AppConfig{
   acHost :: String,
@@ -37,6 +43,9 @@ data FancyAppConfig f = FancyAppConfig {
 
 data ConfigurationError = ConfigParseError String
                         | TOMLParseError ParseError
+                        | InvalidPath FP.FilePath String
+deriving instance Show ConfigurationError
+deriving instance Exception ConfigurationError
 {-
 {
     "facHost" : "ssss",
@@ -94,15 +103,79 @@ instance FromTOMLFile PartialAppConfig where
                                          Left err          -> Left err
 
 class FromENV cfg where
-  fromEnv :: ProcessEnvironment -> IO (Either ConfigurationError cfg)
+  fromEnv :: ProcessEnvironment -> cfg
 
 instance FromENV PartialAppConfig where
-  fromEnv pEnv = pure $ Right $ FancyAppConfig { facHost = prop "host",
-                                         facPort = join $ fmap readMaybe $ prop "port"
-                                       }
+  fromEnv pEnv = FancyAppConfig { facHost = prop "host",
+                                  facPort = join $ fmap readMaybe $ prop "port"
+                                }
                  where
                    env :: [(String,String)]
                    env = getProcessEnv pEnv
 
                    prop :: String -> Maybe String
                    prop = flip lookup env
+
+class MergeOverridable a where
+  mergeOverride :: a -> a -> a
+
+instance MergeOverridable PartialAppConfig where
+  mergeOverride a b = FancyAppConfig { facHost = resolveMaybes facHost
+                                     , facPort = resolveMaybes facPort
+                                     }
+                      where
+                        resolveMaybes :: (PartialAppConfig -> Maybe a) -> Maybe a
+                        resolveMaybes fn = (fn b) <|> (fn a)
+
+mergeInPartial :: CompleteAppConfig -> PartialAppConfig -> CompleteAppConfig
+mergeInPartial c p = FancyAppConfig { facHost = fromMaybe (facHost c) (Identity <$> facHost p),
+                                      facPort = fromMaybe (facPort c) (Identity <$> facPort p)
+                                    }
+
+rightOrThrow :: Exception a => Either a b -> IO b
+rightOrThrow e = case e of
+                   (Left err) -> throw err
+                   (Right v)  -> return v
+
+buildConfigWithDefault :: CompleteAppConfig -> [PartialAppConfig] -> CompleteAppConfig
+buildConfigWithDefault orig partials = orig `mergeInPartial` combinedPartials
+  where
+    combinedPartials :: PartialAppConfig
+    combinedPartials = Prelude.foldl mergeOverride (defaultValue :: PartialAppConfig) partials
+
+makeAppConfig :: ProcessEnvironment -> FP.FilePath -> IO (Either ConfigurationError CompleteAppConfig)
+makeAppConfig env path = try generateConfig
+  where
+    extension :: Maybe Text
+    extension = FP.extension path
+
+    isJsonExtension :: String -> Bool
+    isJsonExtension = (== "json")
+
+    isTOMLExtension = (== "toml")
+
+    isJSONFile :: Bool
+    isJSONFile = maybe False id $ (isJsonExtension . unpack) <$> extension
+
+    isTOMLFile :: Bool
+    isTOMLFile = maybe False id $ (isTOMLExtension . unpack) <$> extension
+
+    pathExtensionIsInvalid :: Bool
+    pathExtensionIsInvalid = not $ isJSONFile || isTOMLFile
+
+    pathInvalidExtensionErr :: ConfigurationError
+    pathInvalidExtensionErr = InvalidPath path "Path is invalid (must be either .json or .toml patg)"
+
+    envCfg :: PartialAppConfig
+    envCfg = (fromEnv env :: PartialAppConfig)
+
+    getFileConfig :: IO (Either ConfigurationError PartialAppConfig)
+    getFileConfig = case isJSONFile of
+                      True  -> fromJSONFile (FCOS.encodeString path)
+                      False -> fromJSONFile (FCOS.encodeString path)
+
+    generateConfig :: IO CompleteAppConfig
+    generateConfig = when pathExtensionIsInvalid (throw pathInvalidExtensionErr)
+                     >> getFileConfig
+                     >>= rightOrThrow
+                     >>= \fileCfg -> pure (buildConfigWithDefault (defaultValue :: CompleteAppConfig)[fileCfg, envCfg])
