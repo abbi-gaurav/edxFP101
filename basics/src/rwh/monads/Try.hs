85733eb293043816ca378c8820b3f629e470dd8d
Gaurav Abbi <abbi.gaurav@gmail.com>
2018-12-25 16:14:12 +0100

    Add a monad example

---
+++ basics/src/rwh/monads/Try.hs
@@ -0,0 +1,33 @@
+module Try where
+
+import           Control.Monad (ap, liftM)
+
+data Expr = Lit Int | Div Expr Expr
+
+data Try a = Err String | Return a
+
+instance Functor Try where
+  fmap = liftM
+
+instance Applicative Try where
+  pure  = return
+  (<*>) = ap
+
+instance Monad Try where
+  return x = Return x
+
+  fail msg = Err msg
+
+  Err e    >>=  _    = Err e
+  Return a >>=  f    = f a
+
+divTry :: Int -> Int -> Try Int
+divTry a b = if b == 0
+             then Err "Div by Zero"
+             else Return (a `div` b)
+
+evalTry :: Expr -> Try Int
+evalTry (Lit a)   = Return a
+evalTry (Div a b) = (evalTry a) >>= \a' ->
+  (evalTry b) >>= \b' ->
+  divTry a' b'
